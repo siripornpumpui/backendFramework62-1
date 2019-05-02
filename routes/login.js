07173427c14359dad9607d7229901e74e0cca938
siripornpumpui <pumpuisiriporn31@gmail.com>
2019-05-02 10:23:30 +0700

    Add generate jwt.

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const MongoClient = require("mongodb").MongoClient;
+const jwt = require("jsonwebtoken");
 
 // Test commit
 router.get("/", (req, res) => {
@@ -9,29 +10,53 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/c", (req, res) => {
-  var nameVar = req.body.name; // name คือตัวที่พิมลง postman
+router.post("/login", (req, res) => {
+  var nameVar = req.body.username; // name คือตัวที่พิมลง postman
   var passwordVar = req.body.password; // password พิมค่าลง postman
   MongoClient.connect(
-    "mongodb://framework:framework62@ds147096.mlab.com:47096/frameworkdb",
-    {
+    "mongodb+srv://weerayut:22374736@cluster0-4wunc.gcp.mongodb.net/newDatabase62?retryWrites=true", {
       useNewUrlParser: true
     },
-    function(err, db) {
-      let dbo = db.db("frameworkdb");
-      let userObj = {
-        username: nameVar, // username คือค่า attribute ใน db
-        password: passwordVar // password คือค่า attribute ใน db
-      };
-      dbo
-        .collection("userLoginTable")
-        .insertOne(userObj, function(err, result) {
-          if (err) throw err;
+    function (err, db) {
+      if (err) {
+        res.sendStatus(404);
+        return;
+      }
+      let dbo = db.db("newDatabase62");
+      dbo.collection("userLoginTable").findOne({
+        username: nameVar,
+        password: passwordVar
+      }, function (err, user) {
+        if (err) {
           res.send({
-            status: "store success",
-            name: nameVar //response
+            status: false
           });
-        });
+        }
+        if (user) {
+          var jwtBearerToken = jwt.sign({
+            id: user._id,
+            rank: user.rank,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            id_mil: user.id_mil,
+            unit_name: user.unit_name,
+            username: user.username
+          }, 'secret', {
+            expiresIn: "365d", // 1 Year
+            subject: "JWT"
+          });
+          res.send(
+            JSON.stringify({
+              status: true,
+              token: jwtBearerToken
+            })
+          );
+        } else {
+          res.send({
+            status: false
+          });
+        }
+      });
       db.close();
     }
   );
@@ -46,11 +71,10 @@ router.post("/register", (req, res) => {
   var nameVar = req.body.username; // name คือตัวที่พิมลง postman
   var passwordVar = req.body.password; // password พิมค่าลง postman
   MongoClient.connect(
-    "mongodb+srv://weerayut:22374736@cluster0-4wunc.gcp.mongodb.net/newDatabase62?retryWrites=true",
-    {
+    "mongodb+srv://weerayut:22374736@cluster0-4wunc.gcp.mongodb.net/newDatabase62?retryWrites=true", {
       useNewUrlParser: true
     },
-    function(err, db) {
+    function (err, db) {
       if (err) {
         res.sendStatus(404);
         return;
@@ -65,40 +89,35 @@ router.post("/register", (req, res) => {
         username: nameVar, // username คือค่า attribute ใน db
         password: passwordVar // password คือค่า attribute ใน db
       };
-      dbo.collection("userLoginTable").findOne(
-        {
-          username: nameVar
-        },
-        function(err, result) {
-          if (err) {
-            res.send({
-              status: false
-            });
-          }
-          console.log(result);
-
-          if (result) {
-            res.send({
-              status: false,
-              message: "มี username นี้แล้ว"
-            });
-          } else {
-            dbo
-              .collection("userLoginTable")
-              .insertOne(userObj, function(err, result) {
-                if (err) {
-                  res.send({
-                    status: false
-                  });
-                }
-                res.send({
-                  status: "store success",
-                  name: nameVar //response
-                });
-              });
-          }
+      dbo.collection("userLoginTable").findOne({
+        username: nameVar
+      }, function (err, result) {
+        if (err) {
+          res.send({
+            status: false,
+            message: err.message
+          });
         }
-      );
+        console.log(result);
+        if (result) {
+          res.send({
+            status: false,
+            message: "มี username นี้แล้ว"
+          });
+        } else {
+          dbo.collection("userLoginTable").insertOne(userObj, function (err, result) {
+            if (err) {
+              res.send({
+                status: false,
+                message: err.message
+              });
+            }
+            res.send({
+              status: true
+            });
+          });
+        }
+      });
       db.close();
     }
   );
@@ -108,22 +127,20 @@ router.post("/login", (req, res) => {
   var nameVar = req.body.username; // name คือตัวที่พิมลง postman
   var passwordVar = req.body.password; // password พิมค่าลง postman
   MongoClient.connect(
-    "mongodb+srv://weerayut:22374736@cluster0-4wunc.gcp.mongodb.net/newDatabase62?retryWrites=true",
-    {
+    "mongodb+srv://weerayut:22374736@cluster0-4wunc.gcp.mongodb.net/newDatabase62?retryWrites=true", {
       useNewUrlParser: true
     },
-    function(err, db) {
+    function (err, db) {
       if (err) {
         res.sendStatus(404);
         return;
       }
       let dbo = db.db("newDatabase62");
-      dbo.collection("userLoginTable").findOne(
-        {
+      dbo.collection("userLoginTable").findOne({
           username: nameVar,
           password: passwordVar
         },
-        function(err, result) {
+        function (err, result) {
           console.log(result);
 
           if (err) {
@@ -143,6 +160,27 @@ router.post("/login", (req, res) => {
         }
       );
       db.close();
+    }
+  );
+});
+router.post('/verifyToken', (req, res) => {
+  jwt.verify(
+    req.body.token,
+    'secret', {},
+    function (err, payload) {
+      if (payload) {
+        res.send(
+          JSON.stringify({
+            verify: true
+          })
+        );
+      } else {
+        res.send(
+          JSON.stringify({
+            verify: false
+          })
+        );
+      }
     }
   );
 });
